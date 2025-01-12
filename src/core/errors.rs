3ab41abfa64a48801a10ace05401e554761df302
Simon Rechermann <simon.rechermann@gmail.com>
2025-01-12 12:15:54 +0100

    Impl some traits from the ConnectionError

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Display};
+
 /// A central error enum for connection-related errors.
 #[derive(Debug)]
 pub enum ConnectionError {
@@ -20,3 +22,15 @@ impl From<serialport::Error> for ConnectionError {
         ConnectionError::PortError(err.to_string())
     }
 }
+
+impl Display for ConnectionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ConnectionError::IoError(e) => write!(f, "IO error: {}", e),
+            ConnectionError::PortError(msg) => write!(f, "Port error: {}", msg),
+            ConnectionError::Other(msg) => write!(f, "Other error: {}", msg),
+        }
+    }
+}
+
+impl std::error::Error for ConnectionError {}

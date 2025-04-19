9a28cb91aff4220b8764235f34a3addd0ee233b4
Simon Rechermann <simon.rechermann@gmail.com>
2025-04-19 10:40:40 +0200

    Added putty proto interface

--- putty_grpc_server/build.rs
+++ putty_grpc_server/build.rs
@@ -2,7 +2,7 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(false) // we only need the server side
-        // .out_dir("src/")              // generated code goes into src/
-        .compile_protos(&["proto/terminal.proto"], &["proto"])
+        .out_dir("src/")              // generated code goes into src/
+        .compile_protos(&["proto/putty_interface.proto"], &["proto"])
         .unwrap();
 }
